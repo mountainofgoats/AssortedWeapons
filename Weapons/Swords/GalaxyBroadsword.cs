35dbe3becdc732955ef1d7b48d94bd78af70f615
Mak0-Z <47305558+Mak0-Z@users.noreply.github.com>
2019-03-08 15:50:10 +0300

    Add files via upload

--- Weapons/Swords/GalaxyBroadsword.cs
+++ Weapons/Swords/GalaxyBroadsword.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace AssortedWeapons.Weapons
+namespace AssortedWeapons.Weapons.Swords
 {
     public class GalaxyBroadsword : ModItem
     {
--- Weapons/Swords/YouSword.cs
+++ Weapons/Swords/YouSword.cs
@@ -2,7 +2,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace AssortedWeapons.Weapons
+namespace AssortedWeapons.Weapons.Swords
 {
     public class YouSword : ModItem
     {
